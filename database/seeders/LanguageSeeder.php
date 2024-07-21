a9baf734c0822d462d66aed0655a2644b13454f4
myuta <myuta.dev@gmail.com>
2024-07-21 15:27:42 +0700

    set language seeder

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LanguageSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $params = [
+            ['language_code' => 'en-US', 'name' => 'English (US)'],
+            ['language_code' => 'en-GB', 'name' => 'English (British)'],
+            ['language_code' => 'zh-CN', 'name' => 'Chinese (Simplified)'],
+            ['language_code' => 'zh-TW', 'name' => 'Chinese (Traditional)'],
+            ['language_code' => 'ja', 'name' => 'Japanese'],
+            ['language_code' => 'ko', 'name' => 'Korean'],
+            ['language_code' => 'es', 'name' => 'Spanish'],
+            ['language_code' => 'fr', 'name' => 'French'],
+            ['language_code' => 'pt', 'name' => 'Portuguese'],
+            ['language_code' => 'de', 'name' => 'German'],
+            ['language_code' => 'it', 'name' => 'Italian']
+        ];
+
+        DB::table('languages')->insert($params);
     }
 }
